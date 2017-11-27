8d36fbf20e5503212431c40eabe4891141b4bef8
StevenS125 <sts12589@gmail.com>
2017-11-27 16:46:45 -0500

    initial documents

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+import { CarrotService } from './services/carrot.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.scss' ],
+  encapsulation: ViewEncapsulation.None
+})
+export class AppComponent {
+
+
+
+}
